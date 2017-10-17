cf2af11300c77324ade1610c3ad184cca9d03f59
Alex Paiva <alex.paiva.934@my.csun.edu>
2017-10-17 10:03:58 -0700

    Adding Unity Test Framework
    Adding Unity Test Framework - Alex Paiva

--- MainProject/WWVB/build/default/production/mcc_generated_files/interrupt_manager.o.d
+++ MainProject/WWVB/build/default/production/mcc_generated_files/interrupt_manager.o.d
@@ -1,3 +1,2 @@
 build/default/production/mcc_generated_files/interrupt_manager.o:  \
- mcc_generated_files/interrupt_manager.c  \
-
+ mcc_generated_files/interrupt_manager.c 
--- MainProject/WWVB/build/default/production/mcc_generated_files/mcc.o.d
+++ MainProject/WWVB/build/default/production/mcc_generated_files/mcc.o.d
@@ -4,5 +4,4 @@ mcc_generated_files/mcc.h  \
  mcc_generated_files/pin_manager.h  \
 mcc_generated_files/i2c1.h  \
  mcc_generated_files/interrupt_manager.h  \
-mcc_generated_files/traps.h  \
-
+mcc_generated_files/traps.h 
